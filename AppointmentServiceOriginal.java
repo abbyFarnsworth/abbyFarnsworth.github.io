3ba00bf66d15b65202121170d6215bf4817e843a
abbyFarnsworth <86175837+abbyFarnsworth@users.noreply.github.com>
2022-02-19 14:14:45 -0500

    Add files via upload

---
+++ AppointmentServiceOriginal.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+/**
+ * @author abby.farnswor_snhu
+ * June 6 2021
+ * CS320
+ * appointment service program
+ */
+package appointment;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/*assign appointment ID*/
+public class AppointmentService {
+ final private List<Appointment> appointmentList = new ArrayList<>();
+ private String newUniqueId() {
+ return UUID.randomUUID().toString().substring(
+ 0, Math.min(toString().length(), 10));
+ }
+ 
+ /*create new appointment*/
+ public void newAppointment() {
+ Appointment appt = new Appointment(newUniqueId());
+ appointmentList.add(appt);
+ }
+ 
+ /*add date to new appointment*/
+ public void newAppointment(Date date) {
+ Appointment appt = new Appointment(newUniqueId(), date);
+ appointmentList.add(appt);
+ }
+ 
+ /*add description to new appointment*/
+ public void newAppointment(Date date, String description) {
+ Appointment appt = new Appointment(newUniqueId(), date, description);
+ appointmentList.add(appt);
+ }
+ 
+ /*delete appointment by appointment ID*/
+ public void deleteAppointment(String id) throws Exception {
+ appointmentList.remove(searchForAppointment(id));
+ }
+ 
+ /*exception thrown if appointment ID is not valid*/
+ protected List<Appointment> getAppointmentList() { return appointmentList; }
+ private Appointment searchForAppointment(String id) throws Exception {
+ int index = 0;
+ while (index < appointmentList.size()) {
+ if (id.equals(appointmentList.get(index).getAppointmentId())) {
+ return appointmentList.get(index);
+ }
+ index++;
+ }
+ throw new Exception("This appointment does not exist.");
+ }
+}
+
